2aa75fc997ccff62656a1f83c120658d26dcbbfb
James Pederson <james@jpederson.com>
2023-03-30 00:32:28 +0000

    Fix category filtering on post list component.

--- library/component/post-list.php
+++ library/component/post-list.php
@@ -4,7 +4,7 @@ function the_post_list() {
 
     // get post list specs
     $title = get_sub_field('title');
-    $category = get_sub_field('category');
+    $category = implode( ',', get_sub_field('category') );
     $posts_per_page = get_sub_field('posts_per_page');
 
     if ( !empty( $category ) ) {
